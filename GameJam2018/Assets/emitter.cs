d59d25f3214a11910c2725b8f60a47d236102d48
Unknown <mi17@umbc.edu>
2018-03-11 13:01:00 -0400

    added metaball vreep

---
+++ GameJam2018/Assets/emitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emitter : MonoBehaviour {
+	public GameObject go;
+	float timer;
+	public int rate;
+	// Use this for initialization
+	void Start () {
+		timer = rate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			timer = rate;
+			Object.Instantiate (go, transform.position+Random.insideUnitSphere * 1, transform.rotation, null);
+		}
+	}
+}
