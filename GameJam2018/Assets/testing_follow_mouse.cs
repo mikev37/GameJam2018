ae2d88ed3a35ac6d012d8e3234507af377408d6e
Benjamin Zeigler <sololegends@users.noreply.github.com>
2018-03-11 15:24:52 -0400

    Turrets are useful!
    Turrets detect, follow, and shoot at nearby enemies. Closest one first

---
+++ GameJam2018/Assets/testing_follow_mouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing_follow_mouse : MonoBehaviour {
+
+	bool following = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnMouseDown(){
+		following = !following;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(following){
+			Vector3 mouse = Input.mousePosition;
+			mouse = Camera.main.ScreenToWorldPoint (mouse);
+			mouse.z = transform.position.z;
+
+			transform.position = mouse;
+		}
+	}
+}
